f876f0763471a1e7b248366bbf552f8c698ac8c1
Olivia Asch <olivia.c.asch@gmail.com>
2021-05-11 10:33:36 +0100

    Use passed username in TwitterDataProvider

--- src/data-providers/TwitterDataProvider.ts
+++ src/data-providers/TwitterDataProvider.ts
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 import { DataProvider } from './interfaces';
 
-export const TwitterDataProvider: DataProvider = async () => {
+export const TwitterDataProvider: DataProvider = async (username: string) => {
   const response = await axios.get(
-    '/.netlify/functions/fetch-twitter-data?username=olivvysaur'
+    `/.netlify/functions/fetch-twitter-data?username=${username}`
   );
 
   const { ownData, friends } = response.data;
